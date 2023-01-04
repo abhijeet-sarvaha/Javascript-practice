2e54dd2e87adbad964bc2cb0eb1eaa4e93c518fb
Abhijeet Mamidwar <abhijeet.mamidwar@sarvaha.com>
2023-01-04 15:07:23 +0530

    Trial commit

---
+++ temp.js
@@ -0,0 +1,46 @@
+class tom {
+    constructor(Name){
+        this.TomName = Name
+    }
+
+}
+
+
+const T = new tom("Abhi")
+console.log(T.TomName);
+
+
+// var arr = [1,2 ,3 ,4 , 5, 5, 6]
+// const [a, b, , , d] = [...arr]
+
+
+// // console.log(a, b, d);
+
+
+// const func = function(a, b, c, d) {
+//     let arr = [a,b,c,d]
+//     return arr.reduce(x => x + 2, 0)
+// }
+
+
+// console.log(func(1,2,3,4));
+
+
+
+
+
+// var dog = {
+//     1 : "",
+//     2 : "Tom"
+// }
+
+
+// function check(Obj, prp) {
+//     if (Obj.hasOwnProperty(prp)) {
+//         return true
+//     }
+//     return false
+// }
+
+
+// console.log(check(dog, "1"));
