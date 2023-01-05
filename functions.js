dc3fbdcc574e875a441c72dd37b70efd185e406d
Abhijeet Mamidwar <abhijeet.mamidwar@sarvaha.com>
2023-01-05 12:14:15 +0530

    Basic function types are done
    learnt about arrow functions

---
+++ functions.js
@@ -0,0 +1,37 @@
+
+// Normal functions
+console.log(F === undefined);
+console.log(A === undefined);
+
+
+// Types of function declaration
+function A(n) {
+    var t = 20;
+    console.log(t);
+    return 20 * n
+}
+
+
+console.log(A(2));
+
+
+var F = function (n) {
+    console.log(n);
+}
+
+console.log(F);
+console.log(A);
+
+
+
+// Arrow functions
+var Arr = (n) => {
+    return n * n
+}
+
+console.log(Arr(5));
+
+
+var temp = () => "Nice to meet you"
+
+console.log(temp());
