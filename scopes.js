bcef64a2fee7d4eb0216722330ebe86a27e2027d
Abhijeet Mamidwar <abhijeet.mamidwar@sarvaha.com>
2023-01-05 14:53:08 +0530

    no change

--- scopes.js
+++ scopes.js
@@ -1,7 +1,14 @@
+var X = 10
+
+
+
 function O() {
+    var X = 5
+    console.log(X);
     console.log("First level function");
     S()
     function S() {
+        console.log();
         console.log("Nested functions");
     }
 }
