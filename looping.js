ab89001e202bcb6cae8d011077ce51166c7512df
Abhijeet Mamidwar <abhijeet.mamidwar@sarvaha.com>
2023-01-05 12:18:49 +0530

    Done with loooping

---
+++ looping.js
@@ -0,0 +1,14 @@
+//  FOR in JS
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+var i = 15
+while (i > 10) {
+    console.log(i);
+    i -= 1
+}
+
+
+var arr = [1,2,33,4 ,55,6677 ]
+arr.forEach((i) => console.log(`number is ${i}`))
