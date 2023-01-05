910bbfe2efcdee613a388958533daf8e4b129317
Abhijeet Mamidwar <abhijeet.mamidwar@sarvaha.com>
2023-01-05 12:18:02 +0530

    switch, if-else, if-else if-else

---
+++ conditionals.js
@@ -0,0 +1,35 @@
+// Conditionals
+
+var A = 5
+
+if(A == 10){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+
+
+if (A == 10) {
+    console.log("It is equal to 10")
+}
+else if(A == 5){
+    console.log("It is equal to 5")
+}
+else{
+    console.log("Value not equal to both 5 and 10");
+}
+
+var n = 3
+switch (n) {
+    case 1:
+        console.log("Value is 1");
+        break;
+    case 12:
+        console.log("Value is 12");
+        break;
+    default:
+        console.log("No case is present in the switch statments");
+        break;
+}
