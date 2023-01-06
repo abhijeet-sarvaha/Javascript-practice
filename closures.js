9700e0d1b62e90de2214767db6b994cd61b117b6
Abhijeet Mamidwar <abhijeet.mamidwar@sarvaha.com>
2023-01-06 14:47:30 +0530

    closures are tried

--- closures.js
+++ closures.js
@@ -1,16 +1,39 @@
-// Functions which maintain their execution context are closures
-function X() {
-    let A = 10
-    function T() {
-        const P = 20;
-        console.log(A, P);
-    }
-    T()
-    function Y() {
-        console.log(A, P);
-    }
-    Y()
-}
+// // Functions which maintain their execution context are closures
 
-const P = 100
-X()
+// for (let i = 1; i < 10; i++) {
+//     function N(i) {
+//         setTimeout(function () {
+//             console.log(i);
+//         }, i * 1000)
+//     }
+//     N(i)  
+// }
+
+
+
+// var x = 1
+// for (let i = 1; i < 10; i++) {
+//     setTimeout(function () {
+//         console.log(i);
+//     }, i * 1000)
+// }
+
+
+
+
+
+// function X() {
+//     let A = 10
+//     function T() {
+//         const P = 20;
+//         console.log(A, P);
+//     }
+//     T()
+//     function Y() {
+//         console.log(A, P);
+//     }
+//     Y()
+// }
+
+// const P = 100
+// X()
