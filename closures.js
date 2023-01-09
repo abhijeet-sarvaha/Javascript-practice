5cd2b718c1695210de27dd245503358a31e79aa4
abhijeet-sarvaha <121998467+abhijeet-sarvaha@users.noreply.github.com>
2023-01-09 16:07:04 +0530

    Hof (#1)
    * Closures on functions
    
    * Diff between function statment and expression
    
    * closures are tried
    
    * SetTimeout
    
    * index.js added script
    
    * Passing parameters to setTimeout function
    
    * Basic HOF

---
+++ closures.js
@@ -0,0 +1,54 @@
+// // Functions which maintain their execution context are closures
+
+
+
+
+// function showTime() {
+//     let dateTime= new Date();
+//     let time = dateTime.toLocaleString();
+//     console.log(time)
+// }
+// setTimeout(showTime, 3000);
+
+// for (let i = 1; i < 10; i++) {
+//     function N(i) {
+//         setTimeout(function () {
+//             console.log(i);
+//         }, i * 1000)
+//     }
+//     N(i)  
+// }
+
+
+
+// var x = 1
+// for (let i = 1; i < 10; i++) {
+//     setTimeout(function () {
+//         console.log(i);
+//     }, i * 1000)
+// }
+
+
+// // parameters to setTimeout
+// function greet(name, lastName) {
+//     console.log(`Hello ${name} ${lastName}`);
+// }
+
+// setTimeout(greet, 1000, 'Abhijeet', 'Mamidwar');
+
+
+// function X() {
+//     let A = 10
+//     function T() {
+//         const P = 20;
+//         console.log(A, P);
+//     }
+//     T()
+//     function Y() {
+//         console.log(A, P);
+//     }
+//     Y()
+// }
+
+// const P = 100
+// X()
---
+++ functions1.js
@@ -0,0 +1,24 @@
+// function Statments
+function S(params) {
+    console.log("Hi this is the function expression");
+}
+
+
+// function expression
+const E = function () {
+    console.log("Hi this is function expression");
+}
+
+
+var T = function P() { // this is named function expression
+    console.log("This is just another type of function expression called as named function expression");
+}
+
+// Anonymous function 
+// function () {
+    
+// }
+// Above function syntax gives error 
+// This type of functions are used when passing as parameter to other function
+
+
